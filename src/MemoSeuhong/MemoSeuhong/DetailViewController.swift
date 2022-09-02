913100b4e983e4ec4654b5e47c17b6cbbc8637a2
hsw1920 <hsw1920@gmail.com>
2022-09-02 19:44:24 +0900

    add) memoDetail (memoCell, dateCell)

---
+++ src/MemoSeuhong/MemoSeuhong/DetailViewController.swift
@@ -0,0 +1,55 @@
+//
+//  DetailViewController.swift
+//  MemoSeuhong
+//
+//  Created by 홍승완 on 2022/09/02.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension DetailViewController: UITableViewDataSource {
+    // tableView가 표시할 cell 수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    // 어떤 cell을 호출 할 것인가?
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // indexPath.row로 몇 번째 cell인지 확인
+        // row는 0부터 시작
+        switch indexPath.row {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "memoCell", for: indexPath)
+            return cell
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell", for: indexPath)
+            return cell
+        default:
+            fatalError()
+        }
+    
+    }
+    
+    
+}
