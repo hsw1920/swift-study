edb3126c8d3e9e4b8927ef3161f9053172858179
hsw1920 <hsw1920@gmail.com>
2022-09-12 21:01:41 +0900

    feat: 회원가입 액션 연결

--- src/Catstagram/Catstagram/RegisterViewController.swift
+++ src/Catstagram/Catstagram/RegisterViewController.swift
@@ -8,22 +8,74 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    // MARK: - Properties
+    
+    // 회원가입을 위한 정보제약조건
+    // 유효성검사를 위한 프로퍼티
+    var isValidEmail = false
+    var isValidName = false
+    var isValidNickName = false
+    var isValidPassword = false
+    
+    // Textfields
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nickNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    // 연산프로퍼티를 통해 한번에 모든 텍스트필드에 접근할 수 있도록 하였음
+    var textFields: [UITextField] {
+        [emailTextField, nameTextField, nickNameTextField, passwordTextField]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTextField()
     }
-    */
 
+    
+    
+    // MARK: - Action
+    
+    // Action을 코드로 구성해보았음
+    @objc
+    func textFieldEditingChanged(_ sender: UITextField) {
+        let text = sender.text ?? ""
+        
+        switch sender {
+        case emailTextField:
+            //print("email: \(sender.text ?? "")") // == text
+            print(text)
+        case nameTextField:
+            print("name: \(sender.text ?? "")")
+        case nickNameTextField:
+            print("nickName: \(sender.text ?? "")")
+        case passwordTextField:
+            print("password: \(sender.text ?? "")")
+        default:
+            fatalError("Missing TextField...")
+        }
+    }
+    
+    
+    
+    // MARK: - Helper
+    
+    private func setupTextField() {
+        // self = RegisterViewController
+        // addTarget은 target이 오면 누가 처리할거냐?
+        // 어떤 action에 대해 누가(for)처리할거냐?
+        // forEach문의 클로저를 활용함
+        textFields.forEach { tf in
+            tf.addTarget(self,
+                         action: #selector(textFieldEditingChanged(_:)),
+                         for: .editingChanged)
+        }
+    }
+    
+    
+    
 }
