d404b0ba51f198628d4faedec2413d5cfc7033f7
hsw1920 <hsw1920@gmail.com>
2022-11-07 21:13:49 +0900

    combine operator

--- src/fc-ios-bible-for-beginner-2022/Part4_Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Operator - combineLatest.xcplaygroundpage/Contents.swift
+++ src/fc-ios-bible-for-beginner-2022/Part4_Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Operator - combineLatest.xcplaygroundpage/Contents.swift
@@ -6,17 +6,69 @@ import Combine
 
 // Basic CombineLatest
 
+let strPublisher = PassthroughSubject<String,Never>()
+let numPublisher = PassthroughSubject<Int,Never>()
 
+Publishers.CombineLatest(strPublisher, numPublisher).sink { (str, num) in
+    print("Receive: \(str), \(num)")
+}
+//strPublisher.combineLatest(numPublisher).sink { (str, num) in
+//    print("Receive: \(str), \(num)")
+//}
 
+//strPublisher.send("a")
+//strPublisher.send("b")
+//strPublisher.send("c")
+//
+//numPublisher.send(1)
+//numPublisher.send(2)
+//numPublisher.send(3)
+
+strPublisher.send("a")
+numPublisher.send(1)
+strPublisher.send("b")
+strPublisher.send("c")
+
+numPublisher.send(2)
+numPublisher.send(3)
 
 
 // Advanced CombineLatest
 
+let usernamePublisher = PassthroughSubject<String,Never>()
+let passwordPublisher = PassthroughSubject<String,Never>()
 
+//Publishers.Merge(usernamePublisher, passwordPublisher)
+//    .map { (username, password) -> Bool in
+//        return !username.isEmpty && !password.isEmpty && password.count>12
+//    }
+//    .sink { valid in
+//        print("Credential valid? : \(valid)")
+//    }
+
+let validatedCredetialsSubscription = usernamePublisher.combineLatest(passwordPublisher)
+    .map { (username, password) -> Bool in
+        return !username.isEmpty && !password.isEmpty && password.count>12
+    }
+    .sink { valid in
+        print("Credential valid? : \(valid)")
+    }
+
+usernamePublisher.send("jasonlee")
+passwordPublisher.send("weakpw")
+passwordPublisher.send("verystrongpassword")
 
 
 // Merge
 
+let publisher1 = [1, 2, 3, 4, 5].publisher
+let publisher2 = [300, 400, 500].publisher
+//let publisher2 = ["300", "400", "500"].publisher
+
+let mergedPublisherSubscription = publisher1.merge(with: publisher2)
+    .sink { value in
+        print("Merge: subscription received value: \(value)")
+    }
 
 
 //: [Next](@next)
--- src/fc-ios-bible-for-beginner-2022/Part4_Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Operator - removeDup & compactMap.xcplaygroundpage/Contents.swift
+++ src/fc-ios-bible-for-beginner-2022/Part4_Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Operator - removeDup & compactMap.xcplaygroundpage/Contents.swift
@@ -6,18 +6,52 @@ import Combine
 var subscriptions = Set<AnyCancellable>()
 
 // removeDuplicates
+// 중복된것 제거
+let words = "hey hey there! Mr Mr ?"
+    .components(separatedBy: " ")
+    .publisher
 
+words
+    .removeDuplicates()
+    .sink { value in
+        print(value)
+    }.store(in: &subscriptions) // -> subscriptions에 저장함
+
+
+let subscription = words // -> subscriptions에 저장함 위와동일
+    .removeDuplicates()
+    .sink { value in
+        print(value)
+    }
 
 // compactMap
+// nil을 제외
 
+let strings = ["a", "1.24", "3", "def", "45", "0.23"].publisher
 
+strings
+    .compactMap{Float($0)}
+    .sink { value in
+        print(value)
+    }.store(in: &subscriptions)
 
 // ignoreOutput
-
+// 새로운것 무시
+let numbers = (1...10_000).publisher
+numbers
+    .ignoreOutput()
+    .sink(receiveCompletion: { print("Completed with: \($0)") },
+          receiveValue: { print($0) })
+    .store(in: &subscriptions)
 
 
 // prefix
+// 들어올 데이터의 앞의 일부분만 받겠다
+let tens = (1...10).publisher
 
-
-
+tens
+    .prefix(5)
+    .sink(receiveCompletion: {print("Completed with: \($0)")},
+          receiveValue: {print($0)})
+    .store(in: &subscriptions)
 //: [Next](@next)
