4eeb4078fd2ffe6877c11670e65ee5ce9d66b218
hsw1920 <hsw1920@gmail.com>
2022-11-08 17:21:19 +0900

    combine 프로젝트 전환

--- src/fc-ios-bible-for-beginner-2022/Part4_Combine/Ch2-Combine_Practice/02_Improve_Project_with_Combine-01/starter/02-AppleFramework_Combine/AppleFramework/FrameworkDetailViewController.swift
+++ src/fc-ios-bible-for-beginner-2022/Part4_Combine/Ch2-Combine_Practice/02_Improve_Project_with_Combine-01/starter/02-AppleFramework_Combine/AppleFramework/FrameworkDetailViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SafariServices
+import Combine
 
 class FrameworkDetailViewController: UIViewController {
     
@@ -14,28 +15,56 @@ class FrameworkDetailViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
-    var framework: AppleFramework = AppleFramework(name: "Unknown", imageName: "", urlString: "", description: "")
+//    @Published var framework: AppleFramework = AppleFramework(name: "Unknown", imageName: "", urlString: "", description: "")
+    
+    // Combine
+    var subscriptions = Set<AnyCancellable>()
+    let buttonTapped = PassthroughSubject<AppleFramework, Never>()
+    let framework = CurrentValueSubject<AppleFramework, Never>(AppleFramework(name: "Unknown", imageName: "", urlString: "", description: ""))
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateUI()
+//        updateUI()
+        bind()
     }
     
-    func updateUI() {
-        imageView.image = UIImage(named: framework.imageName)
-        titleLabel.text = framework.name
-        descriptionLabel.text = framework.description
-    }
+//    func updateUI() {
+//        imageView.image = UIImage(named: framework.imageName)
+//        titleLabel.text = framework.name
+//        descriptionLabel.text = framework.description
+//    }
     
+    private func bind() {
+        // input: Button 클릭
+        buttonTapped
+            .receive(on: RunLoop.main)
+            .compactMap { URL(string: $0.urlString)}
+            .sink { [unowned self] url in
+                let safari = SFSafariViewController(url: url)
+                self.present(safari, animated: true)
+            }.store(in: &subscriptions)
+        
+        // output: Data 세팅될 때 UI 업데이트
+//        $framework
+        framework
+            .receive(on: RunLoop.main)
+            .sink { [unowned self] framework in
+                self.imageView.image = UIImage(named: framework.imageName)
+                self.titleLabel.text = framework.name
+                self.descriptionLabel.text = framework.description
+            }.store(in: &subscriptions)
+    }
     
     @IBAction func learnMoreTapped(_ sender: Any) {
+//        buttonTapped.send(framework)
+        buttonTapped.send(framework.value)
         
-        guard let url = URL(string: framework.urlString) else {
-            return
-        }
-        
-        let safari = SFSafariViewController(url: url)
-        
-        present(safari, animated: true)
+//        guard let url = URL(string: framework.urlString) else {
+//            return
+//        }
+//
+//        let safari = SFSafariViewController(url: url)
+//
+//        present(safari, animated: true)
     }
 }
--- src/fc-ios-bible-for-beginner-2022/Part4_Combine/Ch2-Combine_Practice/02_Improve_Project_with_Combine-01/starter/02-AppleFramework_Combine/AppleFramework/FrameworkListViewController.swift
+++ src/fc-ios-bible-for-beginner-2022/Part4_Combine/Ch2-Combine_Practice/02_Improve_Project_with_Combine-01/starter/02-AppleFramework_Combine/AppleFramework/FrameworkListViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 class FrameworkListViewController: UIViewController {
     
@@ -18,12 +19,68 @@ class FrameworkListViewController: UIViewController {
     
     var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
     
-    let list: [AppleFramework] = AppleFramework.list
+//    let list: [AppleFramework] = AppleFramework.list
+    
+    // Combine
+    var subscriptions = Set<AnyCancellable>()
+    let didSelect = PassthroughSubject<AppleFramework, Never>()
+    
+    // @Published는 var로 바꿔서 선언해줘야함.
+    //@Published var list: [AppleFramework] = AppleFramework.list
+    
+    // @Published 대신 CurrentValueSubject를 이용
+    let items = CurrentValueSubject<[AppleFramework], Never>(AppleFramework.list)
+    
     
     // Data, Presentation, Layout
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // CollectionView Presentation, Layout 설정해주는 것
+        // CollectionView 그리는데 필요한 Data를 설정해주는 것
+        configureCollectionView()
+        bind()
+    }
+    
+    private func bind() {
+        // input: 사용자 input을 받아서 처리해야할 것
+        // - item 선택되었을 때 처리
+        // didselect되었을 때 framework를 받아서 처리함
+        didSelect
+            .receive(on: RunLoop.main)
+            .sink { [unowned self] framework in
+            let sb = UIStoryboard(name: "Detail", bundle: nil)
+            let vc = sb.instantiateViewController(withIdentifier: "FrameworkDetailViewController") as! FrameworkDetailViewController
+//                vc.framework = framework
+                vc.framework.send(framework)
+            self.present(vc, animated: true)
+        }.store(in: &subscriptions)
+        
+        // output: data, state 변경에 따라서 UI 업데이트할 것
+        // - items 세팅이 되었을 때 view(collectionView)를 업데이트
+
+//        $list
+//            .receive(on: RunLoop.main)
+//            .sink { [unowned self] list in
+//            applySectionItems(list)
+//        }.store(in: &subscriptions)
+        
+        items
+            .receive(on: RunLoop.main)
+            .sink { [unowned self] items in
+            applySectionItems(items)
+        }.store(in: &subscriptions)
+    }
+    
+    private func applySectionItems(_ items: [Item], to section: Section = .main) {
+        // data
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([section])
+        snapshot.appendItems(items, toSection: section)
+        dataSource.apply(snapshot)
+    }
+    
+    private func configureCollectionView() {
         // presentation
         dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FrameworkCell", for: indexPath) as? FrameworkCell else {
@@ -32,12 +89,6 @@ class FrameworkListViewController: UIViewController {
             cell.configure(item)
             return cell
         })
-        
-        // data
-        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
-        snapshot.appendSections([.main])
-        snapshot.appendItems(list, toSection: .main)
-        dataSource.apply(snapshot)
         
         // layer
         collectionView.collectionViewLayout = layout()
@@ -67,13 +118,16 @@ class FrameworkListViewController: UIViewController {
 
 extension FrameworkListViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let framework = list[indexPath.item]
+        
+        //let framework = list[indexPath.item]
+        let framework = items.value[indexPath.item]
+        
         print(">>> selected: \(framework.name)")
-        
-        let sb = UIStoryboard(name: "Detail", bundle: nil)
-        let vc = sb.instantiateViewController(withIdentifier: "FrameworkDetailViewController") as! FrameworkDetailViewController
-        vc.framework = framework
-        
-        present(vc, animated: true)
+        didSelect.send(framework)
+//        let sb = UIStoryboard(name: "Detail", bundle: nil)
+//        let vc = sb.instantiateViewController(withIdentifier: "FrameworkDetailViewController") as! FrameworkDetailViewController
+//        vc.framework = framework
+//
+//        present(vc, animated: true)
     }
 }
