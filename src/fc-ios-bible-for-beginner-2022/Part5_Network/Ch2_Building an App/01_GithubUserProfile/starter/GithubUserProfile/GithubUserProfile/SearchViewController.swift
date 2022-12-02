939c69d71c0d92ced9bacca72003b33e7fdfbe3f
hsw1920 <hsw1920@gmail.com>
2022-12-02 16:26:03 +0900

    feat: networkService 리팩토링

--- src/fc-ios-bible-for-beginner-2022/Part5_Network/Ch2_Building an App/01_GithubUserProfile/starter/GithubUserProfile/GithubUserProfile/SearchViewController.swift
+++ src/fc-ios-bible-for-beginner-2022/Part5_Network/Ch2_Building an App/01_GithubUserProfile/starter/GithubUserProfile/GithubUserProfile/SearchViewController.swift
@@ -13,12 +13,14 @@ class UserProfileViewController: UIViewController {
     
     // setupUI
     // userprofile
-    // bind
-    // search control
-    // network
+    // bind -> updateUI
+    // search control -> network
+    
+    let network = NetworkService(configuration: .default)
     
     @Published private(set) var user: UserProfile?
     var subscriptions = Set<AnyCancellable>()
+    
     @IBOutlet weak var thumbnail: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var loginLabel: UILabel!
@@ -56,6 +58,22 @@ class UserProfileViewController: UIViewController {
     }
     
     private func update(_ user: UserProfile?) {
+        // user가 nil이 들어올 수 있음
+        guard let user = user else {
+            self.nameLabel.text = "n/a"
+            self.loginLabel.text = "n/a"
+            self.followerLabel.text = ""
+            self.followingLabel.text = ""
+            self.thumbnail.image = nil
+            return
+        }
+        
+        self.nameLabel.text = user.name
+        self.loginLabel.text = user.login
+        self.followerLabel.text = "follower: \(user.followers)"
+        self.followingLabel.text = "following: \(user.following)"
+        // kingfisher 오픈소스 사용하여 간편화
+        self.thumbnail.kf.setImage(with: user.avatarUrl)
         
     }
     
@@ -71,5 +89,77 @@ extension UserProfileViewController: UISearchResultsUpdating {
 extension UserProfileViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         print("button clicked: \(searchBar.text)")
+        
+        guard let keyword = searchBar.text, !keyword.isEmpty else { return }
+        
+        //"https://api.github.com/users/\(keyword)"
+        
+        // Resource
+        
+        //        let base = "https://api.github.com/"
+        //        let path = "users/\(keyword)"
+        //        let params: [String: String] = [:]
+        //        let header: [String: String] = ["Content-Type": "application/json"]
+        //
+        //        var urlComponents = URLComponents(string: base + path)!
+        //        let queryItems = params.map { (key: String, value: String) in
+        //            return URLQueryItem(name: keyword, value: value)
+        //        }
+        //        urlComponents.queryItems = queryItems
+        //
+        //        var request = URLRequest(url: urlComponents.url!)
+        //        header.forEach { (key: String, value: String) in
+        //            request.addValue(value, forHTTPHeaderField: key)
+        //        }
+        
+        // 위의 만든것을 network를 연결해줘야함
+        
+        let resource = Resource<UserProfile>(
+            base: "https://api.github.com/",
+            path: "users/\(keyword)",
+            params: [:],
+            header: ["Content-Type": "application/json"])
+        
+        // NetworkService
+        network.load(resource)
+            .receive(on: RunLoop.main)
+            .sink { completion in
+                switch completion{
+                case .failure(let error):
+                    self.user = nil
+                case .finished: break
+                }
+            } receiveValue: { user in
+                self.user = user
+            }.store(in: &subscriptions)
+
     }
+//        URLSession.shared
+//            .dataTaskPublisher(for: request)
+//            .tryMap { result -> Data in // 오류나 에러를 먼저 잡아야함
+//                guard let response = result.response as? HTTPURLResponse,
+//                      (200..<300).contains(response.statusCode) else {
+//                    let response = result.response as? HTTPURLResponse
+//                    let statusCode = response?.statusCode ?? -1
+//                    // 잡은 오류를 던져줌
+//                    throw NetworkError.responseError(statusCode: statusCode)
+//                }
+//                // 정상적인 경우 result의 data를 넘겨줌
+//                return result.data
+//            }
+//            .decode(type: UserProfile.self, decoder: JSONDecoder())
+//            .receive(on: RunLoop.main)
+//            .sink { completion in
+//                print("completion: \(completion)")
+//                switch completion{
+//                case .failure(let error):
+//                    self.user = nil
+//                case .finished: break
+//                }
+//            } receiveValue: { user in
+//                self.user = user
+//            }.store(in: &subscriptions)
+
+        
+    //}
 }
