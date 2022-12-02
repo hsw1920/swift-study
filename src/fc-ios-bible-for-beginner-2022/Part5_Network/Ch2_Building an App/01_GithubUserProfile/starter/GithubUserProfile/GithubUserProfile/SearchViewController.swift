67b940b8a48578da9b56095bf72618d2b2abd1d6
hsw1920 <hsw1920@gmail.com>
2022-12-02 15:44:38 +0900

    feat: searchBar 연동

--- src/fc-ios-bible-for-beginner-2022/Part5_Network/Ch2_Building an App/01_GithubUserProfile/starter/GithubUserProfile/GithubUserProfile/SearchViewController.swift
+++ src/fc-ios-bible-for-beginner-2022/Part5_Network/Ch2_Building an App/01_GithubUserProfile/starter/GithubUserProfile/GithubUserProfile/SearchViewController.swift
@@ -11,6 +11,14 @@ import Kingfisher
 
 class UserProfileViewController: UIViewController {
     
+    // setupUI
+    // userprofile
+    // bind
+    // search control
+    // network
+    
+    @Published private(set) var user: UserProfile?
+    var subscriptions = Set<AnyCancellable>()
     @IBOutlet weak var thumbnail: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var loginLabel: UILabel!
@@ -19,5 +27,49 @@ class UserProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        embedSearchControl()
+        bind()
+    }
+    
+    private func setupUI() {
+        thumbnail.layer.cornerRadius = 80
+    }
+    
+    private func embedSearchControl() {
+        self.navigationItem.title = "Search"
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "hsw1920"
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        self.navigationItem.searchController = searchController
+    }
+    
+    private func bind() {
+        $user
+            .receive(on: RunLoop.main)
+            .sink { [unowned self] result in
+                self.update(result)
+            }.store(in: &subscriptions)
+    }
+    
+    private func update(_ user: UserProfile?) {
+        
+    }
+    
+}
+
+extension UserProfileViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let keyword = searchController.searchBar.text
+        print("search: \(keyword)")
+    }
+}
+
+extension UserProfileViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("button clicked: \(searchBar.text)")
     }
 }
