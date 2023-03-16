559c50f0f96c78a64bc21803587abaa1864f390d
hsw1920 <hsw1920@gmail.com>
2023-03-17 00:40:39 +0900

    add TextView

---
+++ src/fc-ios-bible-for-beginner-2022/Part7_SwiftUI/Ch2_SwiftUI-View-이해하기/01-Basic Views/start/SwiftUI-Basic/SwiftUI-Basic/TextView.swift
@@ -0,0 +1,20 @@
+//
+//  TextView.swift
+//  SwiftUI-Basic
+//
+//  Created by 홍승완 on 2023/03/17.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    var body: some View {
+        Text("SwiftUI")
+    }
+}
+
+struct TextView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextView()
+    }
+}
