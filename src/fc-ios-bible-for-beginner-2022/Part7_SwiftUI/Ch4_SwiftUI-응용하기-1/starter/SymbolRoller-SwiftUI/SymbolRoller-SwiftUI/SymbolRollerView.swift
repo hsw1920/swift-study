85d10c7d84ef0864b21bb06391afe571e2502f10
hsw1920 <hsw1920@gmail.com>
2023-03-18 18:21:51 +0900

    finish symbolroller

--- src/fc-ios-bible-for-beginner-2022/Part7_SwiftUI/Ch4_SwiftUI-응용하기-1/starter/SymbolRoller-SwiftUI/SymbolRoller-SwiftUI/SymbolRollerView.swift
+++ src/fc-ios-bible-for-beginner-2022/Part7_SwiftUI/Ch4_SwiftUI-응용하기-1/starter/SymbolRoller-SwiftUI/SymbolRoller-SwiftUI/SymbolRollerView.swift
@@ -10,9 +10,47 @@ import SwiftUI
 struct SymbolRollerView: View {
     
     let symbols: [String] = ["sun.min", "moon", "cloud", "wind", "snowflake"]
+    @State var idx = 0
+    
+    @State var imageName: String = "moon"
     
     var body: some View {
-        Text("Hello Symbol Roller")
+        VStack{
+            Spacer()
+            Image(systemName: imageName)
+                .renderingMode(.template)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding()
+            Spacer()
+            Text(imageName)
+                .font(.system(size: 40, weight: .bold))
+            Button {
+                print("Button Tapped")
+                //imageName = symbols.randomElement()!
+                imageName = symbols[idx]
+                idx+=1
+                if idx >= symbols.count {
+                    idx = 0
+                }
+            } label: {
+                HStack{
+                    Image(systemName: "arrow.3.trianglepath")
+                    VStack{
+                        Text("Reload")
+                            .font(.system(size: 30, weight: .bold, design: .default))
+                        Text("click me to reload")
+                    }
+                }
+            }
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity, minHeight: 80)
+            .background(.pink)
+//            .clipShape(Capsule())
+            .cornerRadius(40)
+//            .padding()
+
+        }
     }
 }
 
