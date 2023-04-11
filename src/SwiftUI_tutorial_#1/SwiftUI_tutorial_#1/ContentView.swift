8ec95b505bbc2f2a0e9a40eadc3d3c0c5d8d979c
hsw1920 <hsw1920@gmail.com>
2023-04-11 23:00:03 +0900

    @State

--- src/SwiftUI_tutorial_#1/SwiftUI_tutorial_#1/ContentView.swift
+++ src/SwiftUI_tutorial_#1/SwiftUI_tutorial_#1/ContentView.swift
@@ -8,15 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isActivated: Bool = false
     var body: some View {
-        HStack{
-            MyVstackView()
-            MyVstackView()
-            MyVstackView()
+        NavigationView {
+            VStack {
+                HStack{
+                    MyVstackView()
+                    MyVstackView()
+                    MyVstackView()
+                }
+                .padding(isActivated ? 50.0 : 10.0)
+                .background(isActivated ? .yellow : .black)
+                // 탭 제스처 추가
+                .onTapGesture {
+                    print("HStack이 클릭됨")
+                    withAnimation {
+                        self.isActivated.toggle()
+                    }
+                }
+                .padding()
+                
+                // 네비게이션 버튼(링크)
+                NavigationLink(destination: MyTextView()) {
+                    Text("네비게이션")
+                        .font(.system(size: 40))
+                        .fontWeight(.bold)
+                        .padding()
+                        .background(.orange)
+                        .foregroundColor(.white)
+                        .cornerRadius(30)
+                }
+                .padding(.top, 50)
+            }
         }
-        .padding()
-        .background(.yellow)
-        
     }
 }
 
@@ -29,15 +53,13 @@ struct ContentView_Previews: PreviewProvider {
 struct MyVstackView: View {
     var body: some View {
         VStack {
-            Text("1")
+            Text("1!")
                 .font(.system(size: 60))
-            Text("2")
+            Text("2!")
                 .font(.system(size: 60))
-            Text("3")
+            Text("3!")
                 .font(.system(size: 60))
         }
-        .background(.red)
-        .padding()
-        .background(.blue)
+        .background(.orange)
     }
 }
